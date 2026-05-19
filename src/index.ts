b966c72989413a78eb355c703ccaaba471e1547c
Petr Herynek <petr.herynek@jtfg.com>
2026-05-19 16:04:16 +0200

    export update

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./enums";
 export * from "./interfaces";
 export * from "./utils/utils";
 export * from "./api";
-export * from "./install/InstallService"
+export * from "./install/InstallService";
+export { default as InstallService } from "./install/InstallService";
